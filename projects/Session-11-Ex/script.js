27ebb4a230c225de57a73326202310ae1c3aa110
Lucas-syss <lucasguerreiroc@outlook.com>
2025-02-14 21:42:06 +0000

    Add files via upload

---
+++ projects/Session-11-Ex/script.js
@@ -0,0 +1,37 @@
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+
+    speak() {
+        return `${this.name} makes a noise.`;
+    }
+
+    static info() {
+        return `I am an animal class.`;
+    }
+}
+
+class Dog extends Animal {
+    constructor(name) {
+        super(name); 
+    }
+
+    speak() {
+        return `${super.speak()} ${this.name} barks!`;
+    }
+}
+
+class Cat extends Animal {
+    constructor(name) {
+        super(name);
+    }
+
+    speak() {
+        return `${super.speak()} MEOW!`;
+    }
+}
+
+const cat = new Cat("Cat");
+const dog = new Dog("Rex");
+console.log(cat.speak(), Animal.info());
