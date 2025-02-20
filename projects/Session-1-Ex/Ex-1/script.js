e60e90c83288a905b2a4204f70a880dafe136e6a
Lucas-syss <lucasguerreiroc@outlook.com>
2025-02-20 21:53:40 +0000

    Delete projects directory

--- projects/Session-1-Ex/Ex-1/script.js
+++
@@ -1,5 +0,0 @@
-function findDuplicates(array) {
-    const duplicates = array.filter((item, index) => array.indexOf(item) !== index);
-    return duplicates;
-}
-console.log(findDuplicates([1,1,1,2,2,3,3,3,4,5,5]));
--- projects/Session-10-Ex/hoisting.js
+++
@@ -1,9 +0,0 @@
-// console.log(hoistedVar); // Output: undefined
-// const hoistedVar = "I'm hoisted!";
-
-// notHoistedFunc(); // Error: notHoistedFunc is not a function
-// var notHoistedFunc = () => console.log("I won't work");
-
- // Output: "I work!"
-fn();
-const fn = function hoistedFunc() { console.log("I work!"); }
